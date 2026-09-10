783c981fc8a90522aeb7d73125d055841de62b92
Ignacio <Ignacio@DESKTOP-G3LN5L9>
2026-09-10 09:59:47 -0300

    cambios

--- sw.js
+++ sw.js
@@ -1,27 +1,42 @@
-const CACHE_NAME = "planes-entrenamiento-v3";
+const CACHE_NAME = "planes-entrenamiento-v5";
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./exercises.json" // <-- Se agrega el JSON a la descarga inicial obligatoria
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
 });
 
+// Cache dinámico: Guarda las imágenes de los ejercicios a medida que el alumno las abre
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) return cachedResponse;
+
+            return fetch(event.request).then(networkResponse => {
+                if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+                    return networkResponse;
+                }
+                const responseToCache = networkResponse.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseToCache);
+                });
+                return networkResponse;
+            });
+        })
+    );
 });
